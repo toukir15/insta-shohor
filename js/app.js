e6b14f113da98b23bbf94e466023237cbb08666f
Md Tanvir Ahmed <tanvir.bd.global@gmail.com>
2023-03-12 14:53:49 +0600

    debug user and text

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-  console.log(id);
   likedPostsId.push(id);
   showPosts(posts);
 };
@@ -52,6 +51,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  console.log(post);
   const image = post.image;
   const userImage = post.userImage;
   const div = document.createElement("article");
@@ -121,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
